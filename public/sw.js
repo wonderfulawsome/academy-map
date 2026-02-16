e627904dfed52a1dd268127bf38e850264ff6a01
Park Sungsoo <150274144+wonderfulawsome@users.noreply.github.com>
2026-02-17 04:23:02 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'academy-map-v1';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
