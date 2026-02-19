fd25a52e58f244106b8e8b376dae4eb1e0fb77f6
Park Sungsoo <150274144+wonderfulawsome@users.noreply.github.com>
2026-02-19 18:28:28 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(r => r.unregister());
+  });
+  caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
